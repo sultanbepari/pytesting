ef958628cd19abeaa6896d8f837e7c04460ac96b
Sultan <sultanbepari2732000@gmail.com>
2021-10-07 08:09:28 +0530

    tested

--- src/app.py
+++ src/app.py
@@ -1,12 +1,28 @@
-from flask import Flask
+import pandas as pd
+from PIL import Image
+import glob
 
-app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "Hello, world!"
+def csv2json():
+    df = pd.read_csv (r'src/csv2json/Name.csv')
+    df.to_json (r'src/csv2json/JSON.js')
+    return 0
+
+
+def jpg2gif():
+    frame_folder = r'src/jpg_to_gif/images'
+    frames = [Image.open(image) for image in glob.glob(f"{frame_folder}/*.jpg")]
+    frame_one = frames[0]
+    frame_one.save("src/jpg_to_gif/my_awesome.gif", format="GIF", append_images=frames,
+                   save_all=True, duration=100, loop=0)
+
+    return 0
 
 
 
 if __name__ == "__main__":
     app.run()
+
+
+
+
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,9 @@
-from app import index
+from app import csv2json, jpg2gif
 
-def test_index():
-    assert index() == "Hello, world!"
+
+def test_index1():
+    assert csv2json() == 0
+
+def test_index2():
+    assert jpg2gif() == 0
 
